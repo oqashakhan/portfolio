97e1bb2d31520933f9bd5b8a217b2bcc4eee1bcf
oqashakhan <khanoqasha002@gmail.com>
2025-02-06 19:02:28 +0500

    deploy

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
                     MY NAME  IS <span>OQASHA</span>
                 </div>
                 <div className="desc">
-                    Lorem ipsum dolor sit amet  consectetur adipisicing elit. Est perferendis aut voluptate laudantium labore eveniet eaque tempora excepturi quo. Alias. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, dicta.
+                    Lorem ipsum dolor  sit  amet  consectetur adipisicing elit. Est perferendis aut voluptate laudantium labore eveniet eaque tempora excepturi quo. Alias. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nemo, dicta.
                 </div>
-                <button>DOWNLOADE CV </button>
+                <button>DOWNLOADE CV  </button>
             </div>
             <div className="avatar">
                 <div className="card">
